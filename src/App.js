68ba03834752ddec21374b2be3067ead1f57a0e2
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-05-07 00:03:04 -0300

    inclusão do gráfico no App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import React, {Component} from "react";
-import {Runtime, Library, Inspector} from "@observablehq/runtime";
-import define from "./chart.js";
+import React from 'react';
+import FanChart from './FanChart.js'
+//import BarChart from './BarChart';
 
-class App extends Component {
-  componentDidMount() {
-    const runtime = new Runtime();
-    //runtime.module(define, Inspector.into(this.animationRef.current));
-    // runtime.module(define, name => {
-    //   if (name === "chart") {
-    //     return new Inspector.into(this.animationRef.current);
-    //   }
-    // })
-
-    runtime.module(define, name => {
-      if (name === "chart") {
-        return new Inspector(document.querySelector("#chart"));
-      }
-    });
-  }
-
-  render() {
+function App() {
     return (
-      <div className="App">
-        <div id="chart"></div>
-      </div>
+        <div>
+            <FanChart chartid={"chart1"}></FanChart>
+            <FanChart chartid={"chart2"}></FanChart>
+            <FanChart chartid={"chart3"}></FanChart>
+        </div>
     );
-  }
 }
 
 export default App;
