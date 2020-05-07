32025d508d04597df6dd4815dffc48f640d591bc
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-05-07 08:55:18 -0300

    criação do componente bar chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import FanChart from './FanChart.js'
-//import BarChart from './BarChart';
+import BarChart from './BarChart';
 
 function App() {
     return (
         <div>
+            <BarChart></BarChart>
             <FanChart chartid={"chart1"}></FanChart>
             <FanChart chartid={"chart2"}></FanChart>
             <FanChart chartid={"chart3"}></FanChart>
