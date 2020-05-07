57a7e2d2f6bbc6d3e7d8dc19fa3d98411002ac8b
Jefersonalves <ferreira.jefersonn@gmail.com>
2020-05-07 00:02:29 -0300

    criação do componente fan chart

---
+++ src/FanChart.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import {Runtime, Library, Inspector} from "@observablehq/runtime";
+import define from "./fanchartObservable.js";
+
+class FanChart extends Component {
+    chartRef = React.createRef();
+
+    componentDidMount() {
+        const runtime = new Runtime();
+
+        runtime.module(define, name => {
+            if (name === "chart") {
+                return new Inspector(this.chartRef.current);
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="FanChart">
+                <div ref={this.chartRef}></div>
+            </div>
+        );
+    }
+}
+
+export default FanChart;
