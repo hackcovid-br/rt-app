a6ac8d466fb455ff89ea9ef43159e96757f9a18b
Bruno Jacob <brunojacob@Brunos-MacBook-Pro.local>
2020-05-08 21:51:29 -0300

    Updates

---
+++ precache-manifest.db4b6b79973aece256a438928d8402c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a85af66c29449edb4c7b07ff4069ea",
+    "url": "/rt-app/index.html"
+  },
+  {
+    "revision": "632d2cd3128dec31c0f4",
+    "url": "/rt-app/static/css/main.d73ff22c.chunk.css"
+  },
+  {
+    "revision": "f7cd29f3fead693d10a3",
+    "url": "/rt-app/static/js/2.257f4b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rt-app/static/js/2.257f4b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632d2cd3128dec31c0f4",
+    "url": "/rt-app/static/js/main.7fc7b402.chunk.js"
+  },
+  {
+    "revision": "a07bc907a1a2e491a6f7",
+    "url": "/rt-app/static/js/runtime-main.6f08c579.js"
+  }
+]);
