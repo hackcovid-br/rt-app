56f495afdb3fbc70c4d6f60749e789fc94b21284
Bruno Jacob <brunojacob@Brunos-MacBook-Pro.local>
2020-05-11 23:36:35 -0300

    Updates

---
+++ precache-manifest.36d573e9405edf8ceb6693d26a1e7e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed7bccc3bc29e94d0d8e45d9c4814c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596958ce8d5f612335c3",
+    "url": "/static/css/main.acbd4097.chunk.css"
+  },
+  {
+    "revision": "295d8856e3ae68b663ca",
+    "url": "/static/js/2.61149c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61149c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596958ce8d5f612335c3",
+    "url": "/static/js/main.843588e7.chunk.js"
+  },
+  {
+    "revision": "081ea82e6ec7793b5004",
+    "url": "/static/js/runtime-main.cca0c8da.js"
+  }
+]);
