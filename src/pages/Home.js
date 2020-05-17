134eb8805784fa15e3433dae0cfa892429b10202
Marco Santana <santannacostamarco@gmail.com>
2020-05-17 10:20:55 -0300

    added semantic elements in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home () {
   return (
     <>
       <h1 className="rtcovid">R<sub>t</sub> Covid-19</h1>
-      <p className="whatsRt">O que é o Rt?</p>
+      <h2 className="whatsRt">O que é o Rt?</h2>
       <p className="answerRt">Rt é o numero de reprodução do vírus no tempo t. Esse número descreve a média de  
       pessoas que são contaminadas por pessoa infectada. A forma mais conhecida dessa métrica é o número básico 
       de reprodução, R0 (r-zero) quando t=0. O problema do R0 é que ele não adapta a mudanças de comportamento ou 
@@ -17,7 +17,7 @@ export default function Home () {
       infectar muita gente.
       Inspirados pelo <a className="link" href="https://github.com/k-sys/covid-19/blob/master/Realtime%20R0.ipynb">notebook</a> do Kevin Systrom, reproduzimos o modelo utilizando os dados do Brasil.
       </p>
-      <p className="States">Comparação entre estados</p>
+      <h3 className="States">Comparação entre estados</h3>
       <p className="latestWeek">Último Dia</p>
       <div className="barchartstyle">
           <BarChart/>
