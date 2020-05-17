5b8b204d4a6e02c29222131aee1a0dd6268f5fe0
Marco Santana <santannacostamarco@gmail.com>
2020-05-17 10:14:28 -0300

    components for footer and header

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer () {
+  return (
+    <div className="Footer">
+        <p className="footertitle">Cálculo R<sub>t</sub></p>
+        <p className="footertitle">Recursos</p>
+        <p className="footertitle">limitações</p>
+    </div>
+  )
+}
