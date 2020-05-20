6e89d3aaf8e1921dc44088a64719f3b86bc9f83b
Marco Santana <santannacostamarco@gmail.com>
2020-05-20 18:30:10 -0300

    display date

--- src/pages/Home/components/CountrySummary/index.jsx
+++ src/pages/Home/components/CountrySummary/index.jsx
@@ -1,11 +1,18 @@
 import "./CountrySummary.scss"
+import moment from "moment";
+
 import BarChart from "../BarChart"
 
 function CountrySummary() {
   return (
     <div className="container country-summary-container">
       <h3 className="title">Comparação entre estados</h3>
-      <p className="data-description">Último Dia</p>
+      <p className="data-description">
+        Dados até:{' '}
+        <span className="featured-text">
+          {moment().format("D/M/YYYY")}
+        </span>
+      </p>
       <div className="chart-wrapper">
         <BarChart />
       </div>
