387f4e30a8e57eda53fbb84bff9326fa940ba014
Bruno Jacob <brunojacob@Brunos-MacBook-Pro.local>
2020-05-21 15:22:39 -0300

    updated content and style

--- src/pages/Home/components/Hero/index.jsx
+++ src/pages/Home/components/Hero/index.jsx
@@ -11,19 +11,12 @@ function Hero() {
         R<sub>t</sub> é o numero de reprodução do vírus no tempo <em>t</em>. Esse número descreve a
         média de pessoas que são contaminadas por pessoa infectada. A forma mais
         conhecida dessa métrica é o número básico de reprodução, R<sub>0</sub> (r-zero)
-        quando t=0. O problema do R0 é que ele não adapta a mudanças de
+        quando t = 0. O problema do R<sub>0</sub> é que ele não adapta a mudanças de
         comportamento ou restrições governamentais e sociais, ele é fixo. Na
-        forma que a pandemia evolui e as restrições aumentam ou diminuem, o R<sub>t</sub>
-        também muda. Quando R<sub>t</sub> {">"} 1, a pandemia espalha para uma porção
-        grande da população. Se o R<sub>t</sub> menor que 1, a pandemia diminui rapidamente
-        antes de infectar muita gente. Inspirados pelo{" "}
-        <a
-          className="link"
-          href="https://github.com/k-sys/covid-19/blob/master/Realtime%20R0.ipynb"
-        >
-          notebook
-        </a>{" "}
-        do Kevin Systrom, reproduzimos o modelo utilizando os dados do Brasil.
+        forma que a pandemia evolui e as restrições aumentam ou diminuem, o R<sub>t</sub> também muda.
+	Quando R<sub>t</sub> é maior que 1, a pandemia espalha para uma porção
+        grande da população. Se o R<sub>t</sub> for menor que 1, a pandemia diminui rapidamente
+        antes de infectar muita gente.
       </p>
     </div>
   )
