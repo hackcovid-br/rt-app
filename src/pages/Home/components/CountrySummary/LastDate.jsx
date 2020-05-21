3e10f5174bed66e591050de307d041c5ceadd2c8
Marco Santana <santannacostamarco@gmail.com>
2020-05-21 11:50:50 -0300

    remove useState

--- src/pages/Home/components/CountrySummary/LastDate.jsx
+++ src/pages/Home/components/CountrySummary/LastDate.jsx
@@ -1,21 +1,20 @@
 import define from "@bbjacob123/rt-chart";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Runtime } from "@observablehq/runtime";
 
 export default function LastDate () {
-  const [lastDate, setLastDate] = useState('');
+  const [state, setState] = useState({
+    lastDate: '',
+  });
 
-  useEffect(() => {
-    const runtime = new Runtime();
-    const module = runtime.module(define);
-    module.value('lastDate').then(value => {
-      const formattedDate = Intl.DateTimeFormat('pt-BR').format(value)
-      setLastDate(formattedDate);
-    });
-  }, []);
-
+  const runtime = new Runtime();
+  const module = runtime.module(define);
+  module.value('lastDate').then(value => {
+    const lastDate = Intl.DateTimeFormat('pt-BR').format(value)
+    setState({...state, lastDate});
+  });
   
   return (
-    <span className="featured-text">{ lastDate }</span>
+    <span className="featured-text">{ state.lastDate }</span>
   )
 }
