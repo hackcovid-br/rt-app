9a4791afa4521a67879fbc1938d4a23772c83e23
Marco Santana <santannacostamarco@gmail.com>
2020-05-24 15:58:48 -0300

    fix last date request repeated several times

--- src/pages/Home/components/CountrySummary/LastDate.jsx
+++ src/pages/Home/components/CountrySummary/LastDate.jsx
@@ -1,18 +1,20 @@
 import define from "@bbjacob123/rt-chart";
 import { Runtime } from "@observablehq/runtime";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function LastDate () {
   const [state, setState] = useState({
     lastDate: '',
   });
 
-  const runtime = new Runtime();
-  const module = runtime.module(define);
-  module.value('lastDate').then(value => {
-    const lastDate = Intl.DateTimeFormat('pt-BR', {timeZone: 'UTC'}).format(value)
-    setState({...state, lastDate});
-  });
+  useEffect(() => {
+    const runtime = new Runtime();
+    const module = runtime.module(define);
+    module.value('lastDate').then(value => {
+      const lastDate = Intl.DateTimeFormat('pt-BR', {timeZone: 'UTC'}).format(value)
+      setState({...state, lastDate});
+    });
+  }, []);
   
   return (
     <span className="featured-text">{ state.lastDate }</span>
