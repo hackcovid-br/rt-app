404f41f07cc68fa10e95cc1943e3dae1ef6206d2
Marco Santana <santannacostamarco@gmail.com>
2020-05-24 17:30:18 -0300

    fix component name

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero"
 import MetaTags from "./components/MetaTags"
 import StatesSummary from "./components/StatesSummary"
 
-export default function Rt() {
+export default function Home() {
   return (
     <div className="home-page-container">
       <MetaTags />
