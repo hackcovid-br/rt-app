519dad42ea400cba6db28159fc5b259cef80e2b2
Bernardo <bernaferrari2@gmail.com>
2020-06-05 22:46:56 -0300

    Revert index that was mistankely added on last commit.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,17 @@ import Hero from "./components/Hero"
 import Apply from "./components/Apply"
 import TalkWithUs from "./components/TalkWithUs"
 import Footer from "./components/Footer"
-import BrHeatChart from "pages/Rt/components/BrHeatChart/BrHeatChart"
 
 function Home() {
   return (
     <Layout>
-      <BrHeatChart></BrHeatChart>
+      <Hero />
+
+      <Apply />
+
+      <TalkWithUs />
+
+      <Footer />
     </Layout>
   )
 }
