1d07f1d92dfc4e75d6c431796782c5cf5faed933
Marco Santana <santannacostamarco@gmail.com>
2020-06-07 17:54:14 -0300

    overlay

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -58,6 +58,8 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+
+      { state.showSidebar && <span className="navbar__overlay" onClick={toggleSidebar} /> }
     </>
   )
 }
