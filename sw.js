9f3480c469ddb42d569986e963e0c75535918200
Bruno Jacob <brunojacob@Brunos-MacBook-Pro.local>
2020-06-12 16:49:25 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,26 +27,26 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-35d0780dc2b9572f1471.js"
+    "url": "webpack-runtime-f12aa49312467299adc5.js"
   },
   {
-    "url": "styles.fb6642cf425effd19675.css"
+    "url": "styles.e897bd5c757868685a81.css"
   },
   {
-    "url": "styles-823ae8103e36ae8a7f9f.js"
+    "url": "styles-9b6f388623a2ec93d35f.js"
   },
   {
-    "url": "framework-d54fcf3f0204b459cf2d.js"
+    "url": "framework-75da9754c2a76bbaf08a.js"
   },
   {
-    "url": "app-ea3be83f6598690a24e9.js"
+    "url": "app-4dfd984f14dbd8e7f4e4.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b158c01f20672e3b5a70718a38f2c4e9"
+    "revision": "b063839e7fd21600b75e9352723e342e"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c25716df4fbb2532700a.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-06e676c3cd7b78941a0b.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ea3be83f6598690a24e9.js`))) {
+  if (!resources || !(await caches.match(`/app-4dfd984f14dbd8e7f4e4.js`))) {
     return await fetch(event.request)
   }
 
