405f0223865f21cf972096d283e20ca31d427400
Marco Santana <santannacostamarco@gmail.com>
2020-06-26 14:36:34 -0300

    Rt page enhancements

--- src/pages/Rt/index.jsx
+++ src/pages/Rt/index.jsx
@@ -2,35 +2,36 @@ import FanChart from "components/FanChart"
 import StatesGrid from 'components/StatesGrid'
 import Layout from "layouts"
 import { useRef } from "react"
+import { Helmet } from "react-helmet"
 
 import CountrySummary from "./components/CountrySummary"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
-import MetaTags from "./components/MetaTags"
-
 
 export default function Rt() {
   const apiLinkRef = useRef(null)
 
   return (
     <Layout>
-      <MetaTags />
+      <Helmet>
+        <title> Rt Covid-19 | HackCovid</title>
+      </Helmet>
 
-        <div className="container main">
-          <Hero apiLinkRef={apiLinkRef} />
-          <h3>Comparação entre estados</h3>
-          <CountrySummary />
+      <div className="container main">
+        <Hero apiLinkRef={apiLinkRef} />
+        <h3>Comparação entre estados</h3>
+        <CountrySummary />
 
-          <h4>
-            Histórico do R<sub>t</sub> por estado
-          </h4>
+        <h4>
+          Histórico do R<sub>t</sub> por estado
+        </h4>
 
-          <StatesGrid render={uf => (
-            <FanChart uf={uf} />) } 
-          />
-        </div>
+        <StatesGrid render={uf => (
+          <FanChart uf={uf} />) } 
+        />
+      </div>
 
-        <Footer apiLinkRef={apiLinkRef} />
+      <Footer apiLinkRef={apiLinkRef} />
 
     </Layout>
   )
