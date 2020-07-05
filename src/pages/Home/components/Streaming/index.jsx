2434cd631d7f287996a5fe6055a9d79f8b26b6cb
Marco Santana <santannacostamarco@gmail.com>
2020-07-05 20:47:07 -0300

    home rt image

--- src/pages/Home/components/Streaming/index.jsx
+++ src/pages/Home/components/Streaming/index.jsx
@@ -1,19 +1,22 @@
 import "./Streaming.scss"
+import image from 'assets/rt.svg';
 import { Link } from 'gatsby';
 
 function Streaming() {
   return (
     <div className="streaming-container">
-        <div className="container">
-            <div className="content-left">
-                <h3 className="content__title">Índice de Transmissão do covid-19 (Rt)</h3>
-                <p className="content__description">É o número médio de pessoas que são infectadas por pessoa contaminada com <span>covid-19</span> .</p>
-                <Link to="/rt" className="default-button">
-                  Confira o índice de sua região
-                </Link>
-            </div>
-            <div className="content-right"></div>
+      <div className="container">
+        <div className="content-left">
+          <h3 className="content__title">Índice de Transmissão do covid-19 (Rt)</h3>
+          <p className="content__description">É o número médio de pessoas que são infectadas por pessoa contaminada com <span>covid-19</span> .</p>
+          <Link to="/rt" className="default-button">
+            Confira o índice de sua região
+          </Link>
         </div>
+        <div className="content-right">
+          <img src={image} alt="transmissão"/>
+        </div>
+      </div>
     </div>
   )
 }
