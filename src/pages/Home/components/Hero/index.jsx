104f544ae842d43fccc8ea76d0de851d9eae1f37
Marco Santana <santannacostamarco@gmail.com>
2020-07-05 16:23:43 -0300

    home hero

--- src/pages/Home/components/Hero/index.jsx
+++ src/pages/Home/components/Hero/index.jsx
@@ -1,17 +1,20 @@
 import "./Hero.scss"
-// import HCLogo from "./HCLogo"
 
-import logo from '../../../../assets/logo2x.svg'
+import logo from 'assets/logo2x.svg'
 
 function Hero() {
   return (
     <div className="hero-container">
       <div className="container">
         <h1 className="hero-title">
-          Empodere-se<span>com ciência e dados</span>
+          <span>
+            Empodere-se 
+          </span>
+          <span>
+            com ciência e dados
+          </span>
         </h1>
         <img src={logo} alt="Logo hack Covid"/>
-        {/* <HCLogo /> */}
       </div>
     </div>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,13 @@
 import Layout from "layouts"
 
-import Hero from "./components/Hero"
-import Because from "./components/Because"
 import Apply from "./components/Apply"
-import Streaming from "./components/Streaming"
-import Mobility from "./components/Mobility"
-import TalkWithUs from "./components/TalkWithUs"
-import Footer from "./components/Footer"
+import Because from "./components/Because"
 import Cases from "./components/Cases"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Mobility from "./components/Mobility"
+import Streaming from "./components/Streaming"
+import TalkWithUs from "./components/TalkWithUs"
 
 function Home() {
   return (
