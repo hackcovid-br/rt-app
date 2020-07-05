18431aceaccf7240814781bc4d11985f4272e414
Marco Santana <santannacostamarco@gmail.com>
2020-07-05 18:13:57 -0300

    home second section 'because'

--- src/pages/Home/components/Because/index.jsx
+++ src/pages/Home/components/Because/index.jsx
@@ -1,36 +1,86 @@
 import "./Because.scss"
 
-import iconeBalance from '../../../../assets/iconeBalance.svg'
-import iconeBlock from '../../../../assets/iconeBlock.svg'
-import IconeShare from '../../../../assets/IconeShare.svg'
-import vectorBashed from '../../../../assets/vectorBashed.svg'
+import iconeBalance from 'assets/iconeBalance.svg'
+import iconeBlock from 'assets/iconeBlock.svg'
+import IconeShare from 'assets/IconeShare.svg'
+import vectorBashed from 'assets/vectorBashed.svg'
+import Highlight from 'components/Highlight';
 
+import { useMaxWidth } from 'hooks';
+
+function BoxItem ({ side, icon, children }) {
+  return (
+    <div className="box__item">
+      { side === "left" && 
+        <p className="content__title">
+          { children }
+        </p>
+      }
+
+      <span className="content__icon">
+        <img src={icon} />
+      </span>
+
+      { side === "right" && 
+        <p className="content__title">
+          { children }
+        </p>
+      }
+    </div>
+  )
+}
+
+function BoxItemBalance ({ side }) {
+  return (
+    <BoxItem side={side} icon={iconeBalance} >
+      Nos dá a direção se devemos <Highlight>aumentar</Highlight> ou {' '}
+      <Highlight>diminuir</Highlight> as restrições equilibrando a prosperidade 
+      econômica e a saúde da população.
+    </BoxItem>
+  )
+}
 
 function Because() {
+
+  const isMobile = useMaxWidth();
+
   return (
     <div className="because-container">
       <div className="container">
         <div className="text-content">
           <h3 className="because-title">
-            Por que é importante <span>conhecer</span> os dados da pandemia do <span>covid-19</span>?
+            Por que é importante <Highlight>conhecer</Highlight> os dados da 
+            pandemia do <Highlight>covid-19</Highlight>?
           </h3>
         </div>
         <div className="box-content">
-            <div className="box__icon">
-                <img src={vectorBashed} alt=""/>
-            </div>
+
+          <div className="box-col left">
+            <BoxItem side="left" icon={IconeShare} >
+              Podemos <Highlight>acompanhar</Highlight> como o <Highlight>vírus</Highlight> está se 
+              espalhando em cada reigião.
+            </BoxItem>
+            { isMobile && 
+              <BoxItemBalance side="left" />
+            }
+            <BoxItem side="left" icon={iconeBlock} >
+              Nos ajuda a <Highlight>entender</Highlight> a efetividade das 
+              medidas de <Highlight>quarentena</Highlight>.
+            </BoxItem>
+          </div>
+
+          <div className="box-col center">
             <div className="box__item">
-                <p className="content__title">Podemos <span>acompanhar</span> como o <span>vírus</span> está se espalhando em cada reigião.</p>
-                <span className="content__icon"><img src={IconeShare} /></span>
+              <img src={vectorBashed} alt=""/>
             </div>
-            <div className="box__item">
-                <span className="content__icon"><img src={iconeBalance} /></span>
-                <p className="content__title">Nos dá a direção se devemos <span>aumentar</span> ou <span>diminuir</span> as restrições equilibrando a prosperidade econômica e a saúde da população.</p>
+          </div>
+
+          { !isMobile && 
+            <div className="box-col right">
+              <BoxItemBalance side="right" />
             </div>
-            <div className="box__item">
-                <p className="content__title">Nos ajuda a <span>entender</span> a efetividade das medidas de <span>quarentena</span>.</p>
-                <span className="content__icon"><img src={iconeBlock} /></span>
-            </div>
+          }
+
         </div>
       </div>
     </div>
