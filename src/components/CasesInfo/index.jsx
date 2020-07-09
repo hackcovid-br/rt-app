4681a0263d2a03df062e77f6cb318e55e6216ab2
joaovsa7 <jvsadesign@gmail.com>
2020-07-09 14:15:29 -0300

    adding image to cases info component

--- src/components/CasesInfo/index.jsx
+++ src/components/CasesInfo/index.jsx
@@ -1,9 +1,18 @@
 import './CasesInfo.scss';
 
 export default function CasesInfo({ label, totalNumber, newNumbers, color }){
+    const imagePaths = {
+        'green': '/icons/head-with-mask.svg',
+        'gray': '/icons/iconDeath.svg',
+        'red': '/icons/iconVirus.svg',
+    };
+
+    const currentImagePath = imagePaths[color];
+
     return (
         <div className="cases-info">
             <div className={`signal ${color}`}>
+                <img src={currentImagePath} alt={`Icone ${label}`} />
             </div>
             <div className="infos">
                 <span className="label">{label}</span>
